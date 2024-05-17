817d8470f7eaf11ff880eba8598d3b57c5cb6deb
Firex18ru <Firex1@yandex.ru>
2024-05-17 22:35:15 +0400

    Исправляю домашнее задание

--- home_work_python2.py
+++ home_work_python2.py
@@ -5,14 +5,14 @@ def test_dark_theme_by_time():
     """
     Протестируйте правильность переключения темной темы на сайте в зависимости от времени
     """
-current_time = time(hour=23)
+    current_time = time(hour=23)
 # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-if current_time.hour >= 22 or current_time.hour < 6:
-    is_dark_theme = True
-else:
-    is_dark_theme = False
+    if current_time.hour >= 22 or current_time.hour < 6:
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
 
-assert is_dark_theme is True
+    assert is_dark_theme is True
 
 
 def test_dark_theme_by_time_and_user_choice():
@@ -23,30 +23,30 @@ def test_dark_theme_by_time_and_user_choice():
     dark_theme_enabled_by_user = False - Темная тема выключена
     dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
     """
-current_time = time(hour=16)
-dark_theme_enabled_by_user = True
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-is_dark_theme = None
+    is_dark_theme = None
 
-if dark_theme_enabled_by_user is True:
-    is_dark_theme = True
-elif dark_theme_enabled_by_user is False:
-    is_dark_theme = False
-elif dark_theme_enabled_by_user is None:
-    if current_time.hour >= 22 or current_time.hour < 6:
+    if dark_theme_enabled_by_user is True:
         is_dark_theme = True
-    else:
+    elif dark_theme_enabled_by_user is False:
         is_dark_theme = False
-assert is_dark_theme is True
+    elif dark_theme_enabled_by_user is None:
+        if current_time.hour >= 22 or current_time.hour < 6:
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
+    assert is_dark_theme is True
 
 
 def test_find_suitable_user():
     """
     Найдите нужного пользователя по условиям в списке пользователей
     """
-users = [
+    users = [
         {"name": "Oleg", "age": 32},
         {"name": "Sergey", "age": 24},
         {"name": "Stanislav", "age": 15},
@@ -55,20 +55,20 @@ users = [
     ]
 
     # TODO найдите пользователя с именем "Olga"
-suitable_users = None
-for user in users:
-    if user["name"] == "Olga":
-        suitable_users = user
+    suitable_users = None
+    for user in users:
+        if user["name"] == "Olga":
+            suitable_users = user
 
-assert suitable_users == {"name": "Olga", "age": 45}
+    assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-suitable_users = []
+    suitable_users = []
 
-for user in users:
-    if user["age"] < 20:
-        suitable_users.append(user)
-assert suitable_users == [
+    for user in users:
+        if user["age"] < 20:
+            suitable_users.append(user)
+    assert suitable_users == [
         {"name": "Stanislav", "age": 15},
         {"name": "Maria", "age": 18},
     ]
@@ -104,6 +104,7 @@ def go_to_companyname_homepage(page_url):
 def find_registration_button_on_login_page(page_url, button_text):
     actual_result = print_function_name_and_args(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
 
 def print_function_name_and_args(func, *args):
     func_name = func.__name__.replace('_', ' ').title()
